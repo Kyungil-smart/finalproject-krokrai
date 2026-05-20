7436aff71bf21b69b1bd43380804d0c5654a5572
krokrai <mks3060@naver.com>
2026-05-20 22:49:26 +0900

    feat : Debug.Log 대체용 Log.Message 생성
    feat : 누락된 new input system 추가

---
+++ Assets/01Scripts/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿/*
+작성자 : krokrai
+작성일 : 26-05-20
+수정일 : 26-05-20
+
+역할 : String Builder에서 미지원 하는 Vector3와 Vector2 지원용
+방식 : String Builder에 기본으로 지원하는 float을 이용해 vector3와 vector2를 출력
+ */
+using System.Text;
+using UnityEngine;
+
+public static class StringBuilderExtensions
+{
+    /// <summary>
+    /// 미지원하는 Vector2를 float으로 변환 후 적용
+    /// </summary>
+    /// <param name="sb"></param>
+    /// <param name="vec"></param>
+    /// <returns></returns>
+    public static StringBuilder AppendVector2(this StringBuilder sb,Vector2 vec)
+    {
+        sb.Append("(")
+            .Append(vec.x)
+            .Append(", ")
+            .Append(vec.y)
+            .Append(")");
+
+        return sb;
+    }
+
+    /// <summary>
+    /// 미지원하는 Vector3를 float으로 변환 후 적용
+    /// </summary>
+    /// <param name="sb"></param>
+    /// <param name="vec"></param>
+    /// <returns></returns>
+    public static StringBuilder AppendVector3(this StringBuilder sb,Vector3 vec)
+    {
+        sb.Append("(")
+            .Append(vec.x)
+            .Append(", ")
+            .Append(vec.y)
+            .Append(", ")
+            .Append(vec.z)
+            .Append(")");
+
+        return sb;
+    }
+}
