23dbbc46ca86f9c881e298007662b1c34a588f6f
krokrai <mks3060@naver.com>
2026-05-21 17:38:42 +0900

    feat : DataBaker 추가 및 SO 추가
    test : 기본적인 검증 완료 즉시 적용 가능

---
+++ Assets/01Scripts/Data/SOScripts/Post_TableSO.cs
@@ -0,0 +1,31 @@
+﻿/*
+ 작성자 : krokrai
+ 작성일 : 26-05-21
+ 수정일 : 26-05-21
+
+ 역할 : 게시물의 정보를 담고 있는 SO
+ */
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Post_TableSO", menuName = "Scriptable Objects/Post_TableSO")]
+public class Post_TableSO : ScriptableObject, ISetSOData
+{
+    public int postID;
+    public ProfileTypeEnum profileType;
+    public int postProFile;
+    public int postImage;
+    public int likeCount;
+    public string captionText;
+    public Route getRoute;
+
+    public void SetData(string[] datas)
+    {
+        datas[0].SetData(out postID);
+        datas[1].SetData<ProfileTypeEnum>(out profileType);
+        datas[2].SetData(out postProFile);
+        datas[3].SetData(out postImage);
+        datas[4].SetData(out likeCount);
+        captionText = datas[5];
+        datas[6].SetData<Route>(out getRoute);
+    }
+}
---
+++ Assets/01Scripts/Interfaces/ISetSOData.cs
@@ -0,0 +1,12 @@
+﻿/*
+작성자 : krokrai
+작성일 : 26-05-21
+수정일 : 26-05-21
+
+역할 : SO에 데이터 주입 용 interface
+방식 : 문자열을 주입하여 SO 내부에서 처리
+*/
+public interface ISetSOData
+{
+    public void SetData(string[] datas);
+}
