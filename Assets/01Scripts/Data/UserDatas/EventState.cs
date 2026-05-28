fc0ea93311f3fa6e3dae8e5028c63daef3769660
krokrai <mks3060@naver.com>
2026-05-28 23:08:54 +0900

    style : 데이터 저장용 class에 header 추가

--- Assets/01Scripts/Data/UserDatas/EventState.cs
+++ Assets/01Scripts/Data/UserDatas/EventState.cs
@@ -1,11 +1,18 @@
-﻿using Firebase.Firestore;
+﻿/*
+ 작성자 : krokrai
+ 작성일 : 26-05-28
+
+ 역할 : FireStore 내부에 EventState 하위에 있는 데이터를 script에 저장
+ 방식 : FireStore에서 정보를 불러온 후 ConvertTo<EventState>()를 해주면, Google 쪽에서 자동으로 scrip에 저장
+ */
+using Firebase.Firestore;
 using System;
 
 [Serializable, FirestoreData]
 public class EventState
 {
     [FirestoreProperty]
-    public int Mission_State { get; set; }
+    public long Mission_State { get; set; }
     [FirestoreProperty]
-    public int Mission_State_Flag { get; set; }
+    public long Mission_State_Flag { get; set; }
 }
--- Assets/01Scripts/Data/UserDatas/Event_Missions.cs
+++ Assets/01Scripts/Data/UserDatas/Event_Missions.cs
@@ -1,4 +1,11 @@
-﻿using Firebase.Firestore;
+﻿/*
+ 작성자 : krokrai
+ 작성일 : 26-05-28
+
+ 역할 : FireStore 내부에 Event_Missions 하위에 있는 데이터를 script에 저장
+ 방식 : FireStore에서 정보를 불러온 후 ConvertTo<Event_Missions>()를 해주면, Google 쪽에서 자동으로 scrip에 저장
+ */
+using Firebase.Firestore;
 using System;
 using System.Collections.Generic;
 
@@ -6,5 +13,6 @@ using System.Collections.Generic;
 public class Event_Missions
 {
     [FirestoreProperty]
-    public Dictionary<string, Dictionary<string, Dictionary<string, EventState>>> Event_490 { get; set; } = new();
+    public Dictionary<string, Dictionary<string, EventState>> Event_490 { get; set; } = new();
+    // 추가 이벤트 시 여기에 위에 줄과 같이 작성 단, firestore의 이름과 변수 명이 정확히 같아야함.
 }
