eec545669ae1783f1eaaa619da199b582c473543
krokrai <mks3060@naver.com>
2026-06-02 16:28:33 +0900

    feat : profilepostcontroller 생성

--- Assets/01Scripts/UI/Profile/ProfileController.cs
+++ Assets/01Scripts/UI/Profile/ProfileController.cs
@@ -5,12 +5,26 @@ using UnityEngine.UI;
 public class ProfileController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _profileName;
+    [SerializeField] private TextMeshProUGUI _postCount;
     [SerializeField] private TextMeshProUGUI _profileFollower;
     [SerializeField] private TextMeshProUGUI _profileFollowing;
     [SerializeField] private Image _profileImage;
 
+    
+
+    private int _currentPostNum;
+    private int _postedCount;
+
     private void OnEnable()
     {
+        _postedCount = ServiceLocator.Get<IDataManager>().UserDatas.UserPost.Count;
+        if (_currentPostNum != _postedCount)
+        {
+            // ProfilePostController 선언 후 생성 요청
+            _currentPostNum = _postedCount;
+            _postCount.text = _currentPostNum.ToString();
+        }
+        
         _profileName.text = ServiceLocator.Get<IDataManager>().ProFile.followerCount.ToString();
         _profileName.text = ServiceLocator.Get<IDataManager>().ProFile.followingCount.ToString();
         ServiceLocator.Get<IAddressableManager>().LoadImageSprite(
---
+++ Assets/01Scripts/UI/Profile/ProfilePostController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfilePostController : MonoBehaviour
+{
+    [SerializeField] private GameObject _postPrefab;
+    
+    public void SetPosts(int num)
+    {
+        // 여기에 이미지 틀 생성 및 주입 예정.
+    }
+}
