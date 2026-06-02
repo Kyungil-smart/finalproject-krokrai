e0ecc2c0560639e657a6d10b553b1ef7f7cfc6ee
krokrai <mks3060@naver.com>
2026-06-02 10:44:00 +0900

    feat : 기본 프로필 구현

---
+++ Assets/01Scripts/UI/Profile/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _profileName;
+    [SerializeField] private TextMeshProUGUI _profileFollower;
+    [SerializeField] private TextMeshProUGUI _profileFollowing;
+    [SerializeField] private Image _profileImage;
+
+    private void OnEnable()
+    {
+        _profileName.text = ServiceLocator.Get<IDataManager>().ProFile.NickName;
+        _profileName.text = ServiceLocator.Get<IDataManager>().ProFile.followerCount.ToString();
+        _profileName.text = ServiceLocator.Get<IDataManager>().ProFile.followingCount.ToString();
+        ServiceLocator.Get<IAddressableManager>().LoadImageSprite(
+            ServiceLocator.Get<IDataManager>().ProFile.profileImage.ToString(),
+            _profileImage);
+    }
+}
