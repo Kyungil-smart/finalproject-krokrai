b50c90e7df4a5c71089d6ac47ec386394ca4e2d8
543xl <543xl@naver.com>
2026-06-05 18:05:27 +0900

    feat/ 팝업UI 구현

---
+++ Assets/01Scripts/UI/PopupUI.cs
@@ -0,0 +1,38 @@
+/*
+작성자 : 이종현
+작성일 : 26-06-04
+수정일 : 26-06-04
+
+역할 : 팝업 활성화 / 비활성화 담당
+방식 : 버튼 클릭 시 지정된 팝업 Canvas를 활성화하거나 비활성화
+*/
+
+using UnityEngine;
+
+public class PopupUI : MonoBehaviour
+{
+    [Header("Popup")]
+    [SerializeField] private GameObject targetPopup;
+
+    public void OpenPopup()
+    {
+        if (targetPopup == null)
+        {
+            Log.Message("Target Popup이 연결되지 않았습니다.");
+            return;
+        }
+
+        targetPopup.SetActive(true);
+    }
+
+    public void ClosePopup()
+    {
+        if (targetPopup == null)
+        {
+            Log.Message("Target Popup이 연결되지 않았습니다.");
+            return;
+        }
+
+        targetPopup.SetActive(false);
+    }
+}
