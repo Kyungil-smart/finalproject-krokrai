8d953a9e1f5c42ed451eaf92b03a6be9054c911a
543xl <543xl@naver.com>
2026-06-08 09:41:01 +0900

    fix : debug 로그 추가

--- Assets/01Scripts/UI/PopupUI.cs
+++ Assets/01Scripts/UI/PopupUI.cs
@@ -35,7 +35,7 @@ public class PopupUI : MonoBehaviour
             Log.Message("TargetPopup이 연결되지 않았습니다.");
             return;
         }
-
+        Log.Message("ClosePopup 호출");
         _targetPopup.SetActive(false);
     }
 
