3e76d4d63813b1aff2cff0271b456c45a5a2dea6
krokrai <mks3060@naver.com>
2026-06-11 10:48:30 +0900

    feat : reels 움직임 구현

---
+++ Assets/01Scripts/UI/ReelsMiniGame/ReelsDrag.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ReelsDrag : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerMoveHandler
+{
+    float _moveYPos;
+    bool _isFirst;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Log.Message(eventData.pressPosition);
+        _moveYPos = eventData.pressPosition.y;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Log.Message(eventData.pressPosition);
+        _isFirst = false;
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if(!_isFirst)
+        {
+            _isFirst = true;
+        }
+        transform.localPosition = new Vector3(0, eventData.position.y - _moveYPos,0);
+        _moveYPos = eventData.position.y;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Log.Message(eventData.pressPosition);
+    }
+}
