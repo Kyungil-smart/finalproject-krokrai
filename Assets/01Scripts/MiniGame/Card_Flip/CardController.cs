efebb8082db1867a5f6b02af5b538afe2a3c8b2d
krokrai <mks3060@naver.com>
2026-06-15 12:50:51 +0900

    feat : card flip game 준비

---
+++ Assets/01Scripts/MiniGame/Card_Flip/CardController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardController : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private SpriteRenderer _sr;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _sr.color = Color.black;
+    }
+}
