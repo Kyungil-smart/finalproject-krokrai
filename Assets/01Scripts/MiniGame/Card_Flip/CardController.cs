f4712d9959157f2804f37d96989af4a88c898261
krokrai <mks3060@naver.com>
2026-06-15 15:23:07 +0900

    feat : card flip 추가

--- Assets/01Scripts/MiniGame/Card_Flip/CardController.cs
+++ Assets/01Scripts/MiniGame/Card_Flip/CardController.cs
@@ -8,13 +8,37 @@ public class CardController : MonoBehaviour, IPointerClickHandler
     [SerializeField] private SpriteRenderer _sr;
     //[SerializeField] private Ctrl;
 
+    private CardGameController _root;
+
+    private bool _isFlip = false;
+
+    private int _index = -1;
+    private int _cardNum = -1;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        _sr.color = Color.black;
+        if (_isFlip) return;
+        _root.ClickedCard(_index,_cardNum);
+        FlipCard();
     }
 
-    public void SetCard()
+    public void SetCard(int index, int cardNum, CardGameController cgc)
     {
+        _index = index;
+        _root = cgc;
+    }
 
+    private void FlipCard()
+    {
+        if(_isFlip)
+        {
+
+        }
+        else // 기본 상태
+        {
+            // 카드 뒤집기
+
+            //
+        }
     }
 }
