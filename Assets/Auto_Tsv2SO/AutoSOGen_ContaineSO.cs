bad2c0c383fafc4bf3af31ddc4ecfcf9a2603550
cminhyeong1999-spec <cminhyeong1999@gmail.com>
2026-06-16 16:41:36 +0900

    chore : AutoTSV 파일 위치 이동, followerReward_kor 프리펩 갱신

--- Assets/Auto_Tsv2SO/AutoSOGen_ContaineSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AutoSOGen_ContaineSO", menuName = "Scriptable Objects/AutoSOGen_ContaineSO")]
-public class AutoSOGen_ContaineSO : ScriptableObject
-{
-    public ScriptableObject[] scriptableObjects;
-}
