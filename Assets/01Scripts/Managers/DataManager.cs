ee795aa0ccae5b7043a2dd24c4ff141d7cce586d
krokrai <mks3060@naver.com>
2026-06-16 11:47:13 +0900

    fix : android 자동 update 해제 및 test code 삭제

--- Assets/01Scripts/Managers/DataManager.cs
+++ Assets/01Scripts/Managers/DataManager.cs
@@ -168,8 +168,6 @@ public class DataManager : MonoBehaviour, IManagerBooter, IDataManager // 현재
                 _userData.Event_Mission.Init();
                 ImageState s = new ImageState();
                 s.getTime = DateTime.Now;
-                _userData.ImgList.Add("502004", s); // TODO : 빌드 후 삭제
-                _userData.UserPost.Add("102001", new());
                 _readyToSave = true;
                 SaveData();
             }
