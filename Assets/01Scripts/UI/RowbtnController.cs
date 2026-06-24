550cd6dada24f3908536bd8f866513ca4cfddb4a
23M-RFT68 <kobh0221@gmail.com>
2026-06-24 15:51:45 +0900

    fix : 하단바 버튼 스크립트에 뒤로가기 버튼 기능 추가 후 수정

--- Assets/01Scripts/UI/RowbtnController.cs
+++ Assets/01Scripts/UI/RowbtnController.cs
@@ -1,3 +1,9 @@
+/*
+ 작성자 : 23M-RFT68
+ 작성일 : 26-06-24
+ 수정일 : 26-06-24
+ 역할  :  하단바 제어 + 홈 이동(돌아가기 버튼) + 알림의 경우 알림 뱃지 해제 추가
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,22 +20,23 @@ public class RowbtnController : MonoBehaviour
         public GameObject activeBg;     // 활성화 보라색 배경 + 흰색 아이콘
     }
     
-    [SerializeField] private TabInfo[] tabs;
-    [SerializeField] private int defaultTab = 0;   // 시작 탭 (0 = 홈피드)
+    [SerializeField] private TabInfo[] _tabs;
+    [SerializeField] private int _defaultTab = 0;            // 시작 탭 (0 = 홈피드)
+    [SerializeField] private GameObject _notiAlertIcon;      // 알림이 왔을때 켜지는 아이콘 뱃지
     
     private int _currentTab = -1;   //  현재 활성화 된 탭
 
     private void Start()
     {
         // 버튼 이벤트 연결
-        for (int i = 0; i < tabs.Length; i++)
+        for (int i = 0; i < _tabs.Length; i++)
         {
             int index = i;  // 클로저 문제 방지
-            tabs[i].button.onClick.AddListener(() => OnTabClicked(index));
+            _tabs[i].button.onClick.AddListener(() => OnTabClicked(index));
         }
         
         // 시작 탭 활성화
-        OnTabClicked(defaultTab);
+        OnTabClicked(_defaultTab);
     }
 
     /// <summary>
@@ -41,8 +48,8 @@ public class RowbtnController : MonoBehaviour
         if (index == 2)
         {
             // 1. 업로드 화면 키기
-            if (tabs[index].page != null)
-                tabs[index].page.SetActive(true);
+            if (_tabs[index].page != null)
+                _tabs[index].page.SetActive(true);
             
             // 2. 하단 바 비활성화
             this.gameObject.SetActive(false);
@@ -53,8 +60,15 @@ public class RowbtnController : MonoBehaviour
         
         if (_currentTab == index) return;    // 이미 활성화 된 탭 클릭 시 무시
         
+        // [알림 뱃지 자동 삭제] 알림 탭 누르면 알림 아이콘 끄기
+        if (_currentTab == 3 && _notiAlertIcon != null)
+        {
+            _notiAlertIcon.SetActive(false);
+            Log.Message("알림 탭 확인 완료: 알림 뱃지 비활성화");
+        }
+
         // 모든 탭 비활성화
-        for (int i = 0; i < tabs.Length; i++)
+        for (int i = 0; i < _tabs.Length; i++)
             SetTabState(i, false);
         
         // 클릭한 탭만 활성화
@@ -73,15 +87,24 @@ public class RowbtnController : MonoBehaviour
         // 직전 탭 상태를 재현하기위해 탭상태 변경 사용
         if (_currentTab != -1)
         {
-            for (int i = 0; i < tabs.Length; i++)
-                SetTabState(i, i ==  _currentTab);
+            for (int i = 0; i < _tabs.Length; i++)
+                SetTabState(i, i == _currentTab);
         }
+    }
+
+    /// <summary>
+    /// 알림 / 릴스 등에서 뒤로가기 버튼 누르면 홈피드로 강제 복귀!
+    /// </summary>
+    public void MoveToHomeFeed()
+    {
+        Log.Message("뒤로가기 클릭 -> 홈피드 화면으로 이동");
+        OnTabClicked(0);
     }
 
     // 탭 상태 변경
     private void SetTabState(int index, bool isActive)
     {
-        TabInfo tab = tabs[index];
+        TabInfo tab = _tabs[index];
         
         if (tab.page != null)
             tab.page.SetActive(isActive);
