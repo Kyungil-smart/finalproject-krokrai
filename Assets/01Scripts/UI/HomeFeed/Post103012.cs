455a9384788204291a3186289cdf4508bdcceec0
cminhyeong1999-spec <cminhyeong1999@gmail.com>
2026-06-29 14:12:07 +0900

    chore : 코드 추가
    - 특정 게시물에서 유저 이름을 참조하는 경우가 있어 이를 위한 코드 임시추가

---
+++ Assets/01Scripts/UI/HomeFeed/Post103012.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Post103012 : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _postText;
+    private string _playerName;
+    
+}
