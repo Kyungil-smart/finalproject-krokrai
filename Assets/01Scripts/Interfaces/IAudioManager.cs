d0f3f8ccea53b12d62f7f5fd7c7e8331059e7c57
krokrai <mks3060@naver.com>
2026-06-29 16:16:10 +0900

    feat : setting prefab 생성 및 slider 추가

--- Assets/01Scripts/Interfaces/IAudioManager.cs
+++ Assets/01Scripts/Interfaces/IAudioManager.cs
@@ -2,7 +2,7 @@
  작성자 : krokrai
  수정자 : 
  작성일 : 26-05-25
- 수정일 : 26-05-25
+ 수정일 : 26-06-29
 
  역할 : Service locator에 등록하기 위한 audio manager interface
  */
@@ -11,5 +11,10 @@ using UnityEngine;
 
 public interface IAudioManager
 {
-    // 여기에 추가
+    public void MuteSFX();
+    public void MuteBGM();
+    public void PlaySFX(string clipName);
+    public void PlayBGM(string clipName);
+    public void SetSFXVolume(float volume);
+    public void SetBGMVolume(float volume);
 }
--- Assets/01Scripts/UI/Setting/SettingsController.cs
+++ Assets/01Scripts/UI/Setting/SettingsController.cs
@@ -1,12 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ 작성자 : krokrai
+ 수정자 : 
+ 작성일 : 26-06-29
+ 수정일 : 
+
+ 역할 : 음량 조절 및 음소거 설정용 제어자
+ 방식 : Slider 및 CheckBox에서 받아온 정보를 기반으로 IAudioManager에 전달.
+*/
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class SettingsController : MonoBehaviour
 {
-    public void SetSFXVolume(float value) => ServiceLocator.Get<AudioManager>().SetSFXVolume(value);
-    public void SetBGMVolume(float value) => ServiceLocator.Get<AudioManager>().SetBGMVolume(value);
+    [SerializeField] Slider _bgmSlider;
+    [SerializeField] Slider _sfxSlider;
 
-    public void SetSFXMute() => ServiceLocator.Get<AudioManager>().MuteSFX();
-    public void SetBGMMute() => ServiceLocator.Get<AudioManager>().MuteBGM();
+    private void Start()
+    {
+        _bgmSlider.onValueChanged.AddListener(SetBGMVolume);
+        _sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+
+        ServiceLocator.Get<IAudioManager>().SetSFXVolume(0.5f);
+        ServiceLocator.Get<IAudioManager>().SetBGMVolume(0.5f);
+    }
+
+    private void OnDestroy()
+    {
+        _bgmSlider.onValueChanged?.RemoveAllListeners();
+        _sfxSlider.onValueChanged?.RemoveAllListeners();
+    }
+
+    public void SetSFXVolume(float value) => ServiceLocator.Get<IAudioManager>().SetSFXVolume(value);
+    public void SetBGMVolume(float value) => ServiceLocator.Get<IAudioManager>().SetBGMVolume(value);
+
+    public void SetSFXMute() => ServiceLocator.Get<IAudioManager>().MuteSFX();
+    public void SetBGMMute() => ServiceLocator.Get<IAudioManager>().MuteBGM();
 }
