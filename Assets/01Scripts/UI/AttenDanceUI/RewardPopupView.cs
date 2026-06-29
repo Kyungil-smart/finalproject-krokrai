12fd04fcb94e9eb46aa095c42fae452f9c6e0730
hiyougg11 <164721514+nekioEmilia@users.noreply.github.com>
2026-06-29 15:47:14 +0900

    fix : 호출 순서 정렬
    특별 감사 사진의 호출 순서를 정렬하고 마지막 출석체크 종료 버튼을 누르면 자동으로 출석체크가 종료되게 만들었습니다

--- Assets/01Scripts/UI/AttenDanceUI/RewardPopupView.cs
+++ Assets/01Scripts/UI/AttenDanceUI/RewardPopupView.cs
@@ -9,12 +9,15 @@
 방식 : Presenter에서 획득한 보상 리스트를 넘겨주면 슬롯 UI 를 갱신하고 팝업을 활성화함
 */
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RewardPopupView : MonoBehaviour
 {
+    private Action _onCloseCallback;
+    
     [SerializeField] private ItemSlotView[] _popupItemSlots;
     [SerializeField] private Button _confirmButton;
     [SerializeField] private ItemDataModel _itemModel;
@@ -26,6 +29,10 @@ public class RewardPopupView : MonoBehaviour
             _confirmButton.onClick.AddListener(() =>
             {
                 gameObject.SetActive(false);
+
+                _onCloseCallback?.Invoke();
+
+                _onCloseCallback = null;
             });
         }
     }
@@ -34,8 +41,10 @@ public class RewardPopupView : MonoBehaviour
     /// 획득한 보상 리스트를 받아와서 팝업창의 슬롯 아이콘과 수량을 갱신하고 팝업을 띄우는 메서드
     /// </summary>
     /// <param name="rewardDatas">획득한 아이템 데이터 리스트</param>
-    public void OpenRewardPopup(List<Reward_Group_TableSO> rewardDatas)
+    public void OpenRewardPopup(List<Reward_Group_TableSO> rewardDatas, Action onClose = null)
     {
+        _onCloseCallback = onClose;
+        
         gameObject.SetActive(true);
 
         for (int i = 0; i < _popupItemSlots.Length; i++)
